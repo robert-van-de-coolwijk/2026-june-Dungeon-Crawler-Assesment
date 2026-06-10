10e829b6a882f423ae25209176d9f2327e18036d
robert-van-de-coolwijk <robert@coolwijk.nl>
2026-06-10 14:41:44 +0200

    Updated entities in preperation for first usage. Added Portal, updated Entity and DefaultEntity

--- dungeon-crawler-project/app/Models/WorldEntities/Portal.php
+++ dungeon-crawler-project/app/Models/WorldEntities/Portal.php
@@ -8,19 +8,18 @@ use App\Models\WorldEntities;
 
 /**
  * MUCK systems call this an "exit", describes the relation between two rooms and can be used to travel between them.
- * In game terms, you can traverse from one room to another allowing some freedom with the portal to describe "the mode of transportation" for flavor
- * and dictates bidirectional by default or one way travel.
+ * In game terms, you can traverse from one room to another allowing some freedom with the portal to describe "the mode of transportation" for flavor.
+ *
+ * portals are ONE way, as:
+ * the name of the portal is what you enter, and doesn't make sense when you appear at the other end.
+ * If you go north, this portal named north.
  */
 class Portal extends WorldEntities\Entity
 {
-    public Boolean $isOneWay;
 
     public Identifier $source;
 
     public Identifier $target;
 
-    public function __construct(){
-        parent::
-    }
 
 }
