0a6207c30404bc26d841c6bbf4bf236c272fea9c
robert-van-de-coolwijk <robert@coolwijk.nl>
2026-06-11 13:30:56 +0200

    Made a MsgWrap(per) that makes it easier to transport messages through the application layers / the system, maintaining semantic intents, but not doing any markup, make up or rendering.

---
+++ dungeon-crawler-project/app/Core/MsgWrapContentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Core;
+
+
+class MsgWrapContentType
+{
+    /**
+     * Heading level 1
+     */
+    public const H1 = 'h1';
+
+    /**
+     * Heading level 2
+     */
+    public const H2 = 'h2';
+
+    /**
+     * Heading level 3
+     */
+    public const H3 = 'h3';
+
+    /**
+     * Paragraph
+     */
+    public const P = 'p';
+
+    /**
+     * Table
+     */
+    public const Table = 'table';
+
+    /**
+     * The content type is NOT set, do not use!
+     */
+    const Unset = 'unset';
+}
---
+++ dungeon-crawler-project/app/Core/MsgWrapSentiment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Core;
+
+
+class MsgWrapSentiment
+{
+    public const Normal = 'normal';
+
+    public const Important = 'important';
+
+    public const Vital = 'vital';
+
+    public const Error = 'error';
+}
