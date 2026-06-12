f13928e6e4662b7ce618fe5d5635125b4c5ff382
robert-van-de-coolwijk <robert@coolwijk.nl>
2026-06-12 14:58:22 +0200

    Expanding on testing through HTTP web.

---
+++ dungeon-crawler-project/public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use App\Controllers\Main;
+use App\Core\MsgWrap\MsgWrap;
+use App\Core\Tools;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// game state monitor
+
+$main = new Main();
+$main->start();
+
+$stateOfTheGameLines = $main->getGameState();
+
+$line = new MsgWrap('');
+
+foreach ($stateOfTheGameLines as $line) {
+    echo sprintf('<%s class=" %s">%s</%s>', $line->contentType, $line->sentiment, $line->msg, $line->contentType);
+}
+
