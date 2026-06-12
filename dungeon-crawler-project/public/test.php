f6fd6aa92d7d0fcb16345e829a88c41586006079
robert-van-de-coolwijk <robert@coolwijk.nl>
2026-06-12 14:56:54 +0200

    Expanding on testing through HTTP web.

---
+++ dungeon-crawler-project/public/test.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use App\Controllers\Main;
+use App\Core\MsgWrap\ContType;
+use App\Core\MsgWrap\MsgWrap;
+use App\Core\Tools;
+use App\Models\WorldEntities\Room;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// game state monitor
+
+$main = new Main();
+$main->start();
+
+$main->command('player', ['Mr Patchouli']);
+
+
+$main->command('init', ['world', 'world_1.txt']);
+
+// RESULT, we do this after we do the testing stuff
+$stateOfTheGameLines = $main->getGameState();
+
+$line = new MsgWrap('');
+
+foreach ($stateOfTheGameLines as $line) {
+    //echo sprintf('<pre class=" %s">%s</pre>', $line->sentiment, $line->msg);
+    $style = '';
+
+    if($line->contentType == ContType::P) {
+        $style .= 'white-space: pre-line;';
+    }
+
+    echo sprintf('<%s class=" %s" style="%s">%s</%s>', $line->contentType, $line->sentiment, $style, $line->msg, $line->contentType);
+}
+
+
+
+//$room = new Room();
+//
+//$room->biome = "forest";
+//$room->name = "unset";
+//$room->description = "unset";
